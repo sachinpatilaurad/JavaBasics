e3ee9e0d577e378091ab04b1adad55eaf4fa8f91
sachin patil <sachinaurad123@gmail.com>
2025-03-28 11:00:53 +0530

    completed the programm

--- src/baiscquestion/PrimeNumber.java
+++ src/baiscquestion/PrimeNumber.java
@@ -1,5 +1,34 @@
 package baiscquestion;
 
-public class PrimeNumber {
+import java.util.Scanner;
 
+public class PrimeNumber
+{
+    public static void main(String[] args)
+    {
+        Scanner inp = new Scanner(System.in);
+        int t = inp.nextInt();
+        for (int i = 0 ; i < t ; i++)
+        {
+            int n = inp.nextInt();
+
+            int count = 0;
+            for (int div = 1 ; div <= n ; div++)
+            {
+                if (n % div == 0)
+                {
+                    count++;
+                }
+
+            }
+            if (count == 2)
+            {
+                System.out.println(n + " it is a prime number");
+            }
+            else
+            {
+                System.out.println("Not a prime");
+            }
+        }
+    }
 }
