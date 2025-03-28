e25ccbafd50361d02622dd3b71c050922f8cfb04
sachin patil <sachinaurad123@gmail.com>
2025-03-28 12:17:19 +0530

    full code

--- src/baiscquestion/PrimeNumTillN.java
+++ src/baiscquestion/PrimeNumTillN.java
@@ -1,2 +1,32 @@
-public class PrimeNumTillN {
+package baiscquestion;
+
+import java.util.Scanner;
+
+public class PrimeNumTillN
+{
+    public static void main(String[] args)
+    {
+        Scanner inp = new Scanner(System.in);
+        int low = inp.nextInt();
+        int high = inp.nextInt();
+
+        for (int i=low; i<=high; i++){
+            int count = 0;
+
+            for (int div = 2;
+                 div * div <= i ; div++){
+                if (i % div == 0){
+                    count ++;
+                    break;
+                }
+
+            }
+            if (count == 0){
+                System.out.println(i);
+            }
+
+        }
+
+    }
 }
+
